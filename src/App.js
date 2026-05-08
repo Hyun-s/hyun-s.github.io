d7bd566e8aed7ad791bbb6706bb41bb07ed58719
Hyun-s <hhs1516@gmail.com>
2026-05-08 22:57:54 +0900

    Fix: Add wildcard route to handle direct URL access

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import CV from './pages/CV';
@@ -16,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/cv" element={<CV />} />
             <Route path="/calendar" element={<Calendar />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </main>
       </div>
